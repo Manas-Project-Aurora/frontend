ce060e7ed2193820374f05de71cc87ca0b34f34b
meyigi <faybmi7@gmail.com>
2025-03-03 23:35:13 +0600

    🧣 width of navbar and cards changed

--- components/VacancyList.vue
+++ components/VacancyList.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="flex flex-col gap-y-0.5">
-    <VacancyListItem
-      v-for="vacancy in vacancies"
-      :key="vacancy.id"
-      :vacancy="vacancy"
-    />
+  <div class="flex justify-center">
+    <div class="flex flex-col gap-y-0.5 mt-12 w-6xl">
+      <VacancyListItem
+        v-for="vacancy in vacancies"
+        :key="vacancy.id"
+        :vacancy="vacancy"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import VacancyListItem from "./VacancyListItem.vue";
+import VacancyListItem from "./VacancyListItem.vue"
 
 defineProps<{
-  vacancies: any[];
-}>();
+  vacancies: any[]
+}>()
 </script>
