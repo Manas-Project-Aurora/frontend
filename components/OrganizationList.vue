04919374d06d63f34024eb71552c1f16a1fdf51e
meyigi <faybmi7@gmail.com>
2025-03-03 23:31:09 +0600

    👽 organizations list and card added

---
+++ components/OrganizationList.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- Контейнер для списка организаций -->
+  <div class="flex justify-center">
+    <div class="flex flex-col gap-y-2 mt-12 w-6xl">
+      <OrganizationListItem
+        v-for="org in organizations"
+        :key="org.id"
+        :organization="org"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import OrganizationListItem from "./OrganizationListItem.vue"
+
+defineProps<{
+  organizations: any[]
+}>();
+</script>
