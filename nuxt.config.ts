ccba07a4c5ca6f8c0688b5c99e074ab3915c8630
meyigi <faybmi7@gmail.com>
2025-04-01 15:15:17 +0600

    💡 configured @primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
+import Aura from "@primeuix/themes/aura"
 
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
@@ -7,4 +8,14 @@ export default defineNuxtConfig({
     plugins: [tailwindcss()],
   },
   css: ["~/assets/css/main.css"],
+  modules: [
+    '@primevue/nuxt-module'
+  ],
+  primevue: {
+    options: {
+        theme: {
+            preset: Aura
+        }
+    }
+}
 });
