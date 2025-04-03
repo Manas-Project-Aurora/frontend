4fb7a006dd6fdb8eee91a25a3653578f20a33183
meyigi <faybmi7@gmail.com>
2025-04-04 00:27:49 +0600

    📟 @organizations --> DataView & Pagination

--- components/OrganizationList.vue
+++ components/OrganizationList.vue
@@ -1,20 +1,42 @@
 <template>
-  <!-- Контейнер для списка организаций -->
   <div class="flex justify-center">
-    <div class="flex flex-col gap-y-2 mt-12 w-6xl">
-      <OrganizationListItem
-        v-for="org in organizations"
-        :key="org.id"
-        :organization="org"
-      />
+    <div class="flex flex-col gap-y-4 mt-12 w-6xl">
+      <DataView 
+        :value="organizations" 
+        paginator 
+        :rows="10" 
+        layout="list"
+        :key="organizations.length"
+      >
+        <template #list="slotProps">
+          <div v-if="slotProps.items.length > 0" class="flex flex-col gap-y-4">
+            <OrganizationListItem 
+              v-for="org in slotProps.items" 
+              :key="org.id" 
+              :organization="org" 
+              @click="goToDetail(org.id)"
+              class="cursor-pointer"
+            />
+          </div>
+          <div v-else class="text-center text-gray-500 mt-4">
+            Нет доступных организаций
+          </div>
+        </template>
+      </DataView>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import OrganizationListItem from "./OrganizationListItem.vue"
+import { useRouter } from 'vue-router';
+import DataView from 'primevue/dataview';
+import OrganizationListItem from "./OrganizationListItem.vue";
 
-defineProps<{
-  organizations: any[]
-}>();
+defineProps<{ organizations: any[] }>();
+const router = useRouter();
+
+const goToDetail = (id: number) => {
+  console.log("Переход к организации ID:", id);
+  router.push(`/organizations/${id}`);
+};
 </script>
