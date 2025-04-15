2c9023c272f03a16852cef493f32ff7f328c672b
Eldos <eldos.baktybekov@gmail.com>
2025-04-16 00:59:38 +0600

    ✨ Enhance organization and vacancy list composables with request parameters for pagination and filtering

--- composables/organizations.ts
+++ composables/organizations.ts
@@ -1,11 +1,20 @@
 import type { OrganizationListResponse } from "~/types/organization"
 
-export const useOrganizationList = () => {
+interface OrganizationListRequestParams {
+  take?: number
+  skip?: number
+}
+
+export const useOrganizationList = ({
+  take,
+  skip,
+}: OrganizationListRequestParams = {}) => {
   const runtimeConfig = useRuntimeConfig()
 
   const url = `${runtimeConfig.public.apiBaseUrl}/v1/organizations/`
   return useFetch<OrganizationListResponse>(url, {
     method: "GET",
     credentials: "include",
+    query: { take, skip },
   })
 }
