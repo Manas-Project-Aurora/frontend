202c087c63054e79ef82521e4f15292b2476b08c
meyigi <faybmi7@gmail.com>
2025-04-16 07:10:19 +0600

    💗 get organization_detail by clicking organization

--- components/data-views/OrganizationListDataView.vue
+++ components/data-views/OrganizationListDataView.vue
@@ -4,37 +4,46 @@
       #list="{ items: organizations }: { items: OrganizationListItem[] }"
     >
       <div class="flex flex-col gap-y-4">
-        <Card v-for="organization in organizations">
-          <template #content>
-            <div class="flex justify-between items-start gap-y-4 sm:items-center flex-col sm:flex-row ">
-              <div class="flex gap-x-4 items-center">
-                <NuxtImg
-                  :src="organization.logo_url"
-                  v-if="organization.logo_url"
-                  width="50"
-                />
-                <div class="flex flex-col text-sm font-semibold">
-                  <p>Компания</p>
-                  <p>{{ organization.name }}</p>
+        <RouterLink
+          v-for="organization in organizations"
+          :key="organization.id"
+          :to="`/organizations/${organization.id}`"
+          class="no-underline"
+        >
+          <Card
+            class="transition-transform hover:shadow-lg hover:-translate-y-1 cursor-pointer"
+          >
+            <template #content>
+              <div class="flex justify-between items-start gap-y-4 sm:items-center flex-col sm:flex-row">
+                <div class="flex gap-x-4 items-center">
+                  <NuxtImg
+                    :src="organization.logo_url"
+                    v-if="organization.logo_url"
+                    width="50"
+                  />
+                  <div class="flex flex-col text-sm font-semibold">
+                    <p>Компания</p>
+                    <p>{{ organization.name }}</p>
+                  </div>
+                </div>
+                <div class="flex text-sm font-semibold gap-x-6">
+                  <div class="flex flex-col">
+                    <p>Вакансии</p>
+                    <p>{{ organization.vacancy_count }}</p>
+                  </div>
+                  <div class="flex flex-col">
+                    <p>Мероприятия</p>
+                    <p>{{ organization.event_count }}</p>
+                  </div>
+                  <div class="flex flex-col">
+                    <p>Видео</p>
+                    <p>{{ organization.video_count }}</p>
+                  </div>
                 </div>
               </div>
-              <div class="flex text-sm font-semibold gap-x-6">
-                <div class="flex flex-col">
-                  <p>Вакансии</p>
-                  <p>{{ organization.vacancy_count }}</p>
-                </div>
-                <div class="flex flex-col">
-                  <p>Мероприятия</p>
-                  <p>{{ organization.event_count }}</p>
-                </div>
-                <div class="flex flex-col">
-                  <p>Видео</p>
-                  <p>{{ organization.video_count }}</p>
-                </div>
-              </div>
-            </div>
-          </template>
-        </Card>
+            </template>
+          </Card>
+        </RouterLink>
       </div>
     </template>
   </DataView>
