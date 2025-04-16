cae8a6e68354ba76a8cb0557d6562f4c337def51
meyigi <faybmi7@gmail.com>
2025-04-16 13:30:19 +0600

    🎪 get vacancy_detail page by clicking vacancy #[2]

--- components/data-views/VacancyListDataView.vue
+++ components/data-views/VacancyListDataView.vue
@@ -1,16 +1,18 @@
 <template>
   <DataView :value="vacancyListResponse.vacancies" data-key="id">
     <template #list="{ items: vacancies }: { items: VacancyListItem[] }">
-      <RouterLink
+      <NuxtLink
         v-for="vacancy in vacancies"
         :key="vacancy.id"
-        :to="`/vacancies/${vacancy.id}`"
+        :to="{ name: 'vacancies-id', params: { id: vacancy.id } }"
         class="no-underline"
       >
         <Card
           class="transition-transform hover:shadow-lg hover:-translate-y-1 cursor-pointer mb-4"
         >
-          <template #title><span class="text-lg">{{ vacancy.title }}</span></template>
+          <template #title>
+            <span class="text-lg">{{ vacancy.title }}</span>
+          </template>
           <template #subtitle>
             <SalaryDetail
               :salary-from="vacancy.salary_from"
@@ -24,18 +26,26 @@
             </section>
           </template>
           <template #footer>
-            <Tag value="Ожидание публикации" severity="warn" v-if="vacancy.status === VacancyStatus.PENDING"/>
-            <Tag value="В архиве" severity="secondary" v-else-if="vacancy.status === VacancyStatus.SUSPENDED"/>
+            <Tag
+              value="Ожидание публикации"
+              severity="warn"
+              v-if="vacancy.status === VacancyStatus.PENDING"
+            />
+            <Tag
+              value="В архиве"
+              severity="secondary"
+              v-else-if="vacancy.status === VacancyStatus.SUSPENDED"
+            />
           </template>
         </Card>
-      </RouterLink>
+      </NuxtLink>
     </template>
   </DataView>
 </template>
 
-
 <script setup lang="ts">
 import { VacancyStatus, type VacancyListItem, type VacancyListResponse } from "~/types/vacancy"
+import { NuxtLink } from '#components'
 
 defineProps<{
   vacancyListResponse: VacancyListResponse
